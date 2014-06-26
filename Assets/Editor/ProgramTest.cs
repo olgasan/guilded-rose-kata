afa5974f717f29d7d600cb194679747557ec1875
Olga Cardenas <oh.cardenas@gmail.com>
2014-06-26 18:52:40 -0500

    Regular Item Decreases One Per Day

---
+++ Assets/Editor/ProgramTest.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using NUnit.Framework;
+
+namespace UnityTest
+{
+	[TestFixture]
+	public class ProgramTest
+	{
+		[Test]
+		public void CanCreateAProgram ()
+		{
+			Item itemA = new Item ();
+			Item itemB = new Item ();
+
+			Program program = new Program (itemA, itemB);
+
+			Assert.IsNotNull (program);
+		}
+
+		[Test]
+		public void RegularItemDecreasesOnePerDay ()
+		{
+			Item item = CreateMockItem ("Elixir of the Mongoose", 5, 7);
+
+			Program program = new Program (item);
+			program.UpdateQuality ();
+
+			Assert.AreEqual (6, item.Quality);
+			Assert.AreEqual (4, item.SellIn);
+		}
+
+		private Item CreateMockItem (string name, int sellIn, int quality)
+		{
+			Item item = new Item ();
+			item.Name = name;
+			item.SellIn = sellIn;
+			item.Quality = quality;
+
+			return item;
+		}
+	}
+}
--- Assets/Scripts/GildedRose.cs
+++ Assets/Scripts/GildedRose.cs
@@ -1,16 +1,27 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
 
-class Program
+public class Program
 {
-	IList<Item> Items;
+	IList<Item> items;
+
+	public Program (params Item[] items)
+	{
+		this.items = new List<Item> ();
+
+		foreach (Item item in items)
+		{
+			this.items.Add (item);
+		}
+	}
+
 	static void Main(string[] args)
 	{
 		Debug.Log("OMGHAI!");
 		
 		var app = new Program()
 		{
-			Items = new List<Item>
+			items = new List<Item>
 			{
 				new Item {Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20},
 				new Item {Name = "Aged Brie", SellIn = 2, Quality = 0},
@@ -33,74 +44,74 @@ class Program
 	
 	public void UpdateQuality()
 	{
-		for (var i = 0; i < Items.Count; i++)
+		for (var i = 0; i < items.Count; i++)
 		{
-			if (Items[i].Name != "Aged Brie" && Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
+			if (items[i].Name != "Aged Brie" && items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
 			{
-				if (Items[i].Quality > 0)
+				if (items[i].Quality > 0)
 				{
-					if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
+					if (items[i].Name != "Sulfuras, Hand of Ragnaros")
 					{
-						Items[i].Quality = Items[i].Quality - 1;
+						items[i].Quality = items[i].Quality - 1;
 					}
 				}
 			}
 			else
 			{
-				if (Items[i].Quality < 50)
+				if (items[i].Quality < 50)
 				{
-					Items[i].Quality = Items[i].Quality + 1;
+					items[i].Quality = items[i].Quality + 1;
 					
-					if (Items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
+					if (items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
 					{
-						if (Items[i].SellIn < 11)
+						if (items[i].SellIn < 11)
 						{
-							if (Items[i].Quality < 50)
+							if (items[i].Quality < 50)
 							{
-								Items[i].Quality = Items[i].Quality + 1;
+								items[i].Quality = items[i].Quality + 1;
 							}
 						}
 						
-						if (Items[i].SellIn < 6)
+						if (items[i].SellIn < 6)
 						{
-							if (Items[i].Quality < 50)
+							if (items[i].Quality < 50)
 							{
-								Items[i].Quality = Items[i].Quality + 1;
+								items[i].Quality = items[i].Quality + 1;
 							}
 						}
 					}
 				}
 			}
 			
-			if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
+			if (items[i].Name != "Sulfuras, Hand of Ragnaros")
 			{
-				Items[i].SellIn = Items[i].SellIn - 1;
+				items[i].SellIn = items[i].SellIn - 1;
 			}
 			
-			if (Items[i].SellIn < 0)
+			if (items[i].SellIn < 0)
 			{
-				if (Items[i].Name != "Aged Brie")
+				if (items[i].Name != "Aged Brie")
 				{
-					if (Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
+					if (items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
 					{
-						if (Items[i].Quality > 0)
+						if (items[i].Quality > 0)
 						{
-							if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
+							if (items[i].Name != "Sulfuras, Hand of Ragnaros")
 							{
-								Items[i].Quality = Items[i].Quality - 1;
+								items[i].Quality = items[i].Quality - 1;
 							}
 						}
 					}
 					else
 					{
-						Items[i].Quality = Items[i].Quality - Items[i].Quality;
+						items[i].Quality = items[i].Quality - items[i].Quality;
 					}
 				}
 				else
 				{
-					if (Items[i].Quality < 50)
+					if (items[i].Quality < 50)
 					{
-						Items[i].Quality = Items[i].Quality + 1;
+						items[i].Quality = items[i].Quality + 1;
 					}
 				}
 			}
