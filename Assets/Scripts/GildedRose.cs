7eb16da8a5aaa75cea2423016002e27389b291f2
Olga Cardenas <oh.cardenas@gmail.com>
2014-06-26 18:28:17 -0500

    Removed Examples and relocated Item class

--- Assets/Scripts/GildedRose.cs
+++ Assets/Scripts/GildedRose.cs
@@ -110,14 +110,4 @@ namespace GildedRose.Console
 		}
 		
 	}
-	
-	public class Item
-	{
-		public string Name { get; set; }
-		
-		public int SellIn { get; set; }
-		
-		public int Quality { get; set; }
-	}
-	
 }
