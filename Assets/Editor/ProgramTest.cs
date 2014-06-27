64ad3cd9d10075eef7a8f11e68d4353235e7225c
Olga Cardenas <oh.cardenas@gmail.com>
2014-06-27 18:40:25 -0500

    Cleaning refactor

--- Assets/Editor/ProgramTest.cs
+++ Assets/Editor/ProgramTest.cs
@@ -6,13 +6,15 @@ namespace UnityTest
 	[TestFixture]
 	public class ProgramTest
 	{
+		private Program program;
+
 		[Test]
 		public void CanCreateAProgram ()
 		{
 			Item itemA = new Item ();
 			Item itemB = new Item ();
 
-			Program program = new Program (itemA, itemB);
+			program = new Program (itemA, itemB);
 
 			Assert.IsNotNull (program);
 		}
@@ -21,101 +23,69 @@ namespace UnityTest
 		public void RegularItemDoesNotChangeWithNegativeQuality ()
 		{
 			Item item = CreateMockItem ("Elixir of the Mongoose", 1, -1);
-			
-			Program program = new Program (item);
-			program.UpdateQuality ();
-			
-			Assert.AreEqual (-1, item.Quality);
-			Assert.AreEqual (0, item.SellIn);
+			program = new Program (item);
 
+			AssertItemValuesAfterOneDay (-1, 0, item);
 		}
-
 
 		[Test]
 		public void RegularItemDecreasesOneQualityPointPerDay ()
 		{
 			Item item = CreateMockItem ("Elixir of the Mongoose", 5, 7);
+			program = new Program (item);
 
-			Program program = new Program (item);
-			program.UpdateQuality ();
-
-			Assert.AreEqual (6, item.Quality, "First day");
-			Assert.AreEqual (4, item.SellIn, "First day");
-
-			program.UpdateQuality ();
-
-			Assert.AreEqual (5, item.Quality, "Second day");
-			Assert.AreEqual (3, item.SellIn, "Second day");
+			AssertItemValuesAfterOneDay (6, 4, item);
+			AssertItemValuesAfterOneDay (5, 3, item);
 		}
 
 		[Test]
 		public void RegularItemDecreasesTwoQualityPointperDayAfterExpires ()
 		{
 			Item item = CreateMockItem ("Elixir of the Mongoose", 0, 2);
-			
-			Program program = new Program (item);
-			program.UpdateQuality ();
-			
-			Assert.AreEqual (0, item.Quality, "last day");
-			Assert.AreEqual (-1, item.SellIn, "last day");
+			program = new Program (item);
 
-			program.UpdateQuality ();
-			
-			Assert.AreEqual (0, item.Quality, "After last day");
-			Assert.AreEqual (-2, item.SellIn, "After last day");
+			AssertItemValuesAfterOneDay (0, -1, item);
+			AssertItemValuesAfterOneDay (0, -2, item);
 		}
 
 		[Test]
 		public void AgedBrieIncreasesOneQualityPointPerDay ()
 		{
 			Item item = CreateMockItem ("Aged Brie", 2, 0);
-			
-			Program program = new Program (item);
-			program.UpdateQuality ();
-			
-			Assert.AreEqual (1, item.Quality, "First day");
-			Assert.AreEqual (1, item.SellIn, "First day");
-			
-			program.UpdateQuality ();
-			
-			Assert.AreEqual (2, item.Quality, "Second day");
-			Assert.AreEqual (0, item.SellIn, "Second day");
-		}
+			program = new Program (item);
 
+			AssertItemValuesAfterOneDay (1, 1, item);
+			AssertItemValuesAfterOneDay (2, 0, item);
+		}
 
 		[Test]
 		public void AgedBrieIncreasesTwoQualityPointperDayAfterExpires ()
 		{
 			Item item = CreateMockItem ("Aged Brie", 0, 0);
-			
-			Program program = new Program (item);
-			program.UpdateQuality ();
-			
-			Assert.AreEqual (2, item.Quality, "last day");
-			Assert.AreEqual (-1, item.SellIn, "last day");
-			
-			program.UpdateQuality ();
-			
-			Assert.AreEqual (4, item.Quality, "After last day");
-			Assert.AreEqual (-2, item.SellIn, "After last day");
+			program = new Program (item);
+
+			AssertItemValuesAfterOneDay (2, -1, item);
+			AssertItemValuesAfterOneDay (4, -2, item);
 		}
 
 		[Test]
 		public void QualityDoesNotChangeWhenIsGreaterThan50 ()
 		{
 			Item item = CreateMockItem ("Aged Brie", 1, 50);
-			
-			Program program = new Program (item);
-			program.UpdateQuality ();
-			
-			Assert.AreEqual (50, item.Quality, "last day");
-			Assert.AreEqual (0, item.SellIn, "last day");
+			program = new Program (item);
+
+			AssertItemValuesAfterOneDay (50, 0, item);
 
 			item.Quality = 77;
-			program.UpdateQuality ();
-			
-			Assert.AreEqual (77, item.Quality, "After last day");
-			Assert.AreEqual (-1, item.SellIn, "After last day");
+			AssertItemValuesAfterOneDay (77, -1, item);
+		}
+
+		private void AssertItemValuesAfterOneDay (int expectedQuality, int expectedSellIn, Item item)
+		{
+			program.UpdateQuality (); //TODO: rename by OnDayAdvanced
+
+			Assert.AreEqual (expectedQuality, item.Quality);
+			Assert.AreEqual (expectedSellIn, item.SellIn);
 		}
 
 		private Item CreateMockItem (string name, int sellIn, int quality)
