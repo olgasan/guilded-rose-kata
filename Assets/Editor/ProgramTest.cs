c2abb03bce4945879b24af8fba0eb2b7fc917d93
Olga Cardenas <oh.cardenas@gmail.com>
2014-06-26 19:01:15 -0500

    [Test]

--- Assets/Editor/ProgramTest.cs
+++ Assets/Editor/ProgramTest.cs
@@ -18,15 +18,32 @@ namespace UnityTest
 		}
 
 		[Test]
-		public void RegularItemDecreasesOnePerDay ()
+		public void RegularItemDecreasesOneQualityPointPerDay ()
 		{
 			Item item = CreateMockItem ("Elixir of the Mongoose", 5, 7);
 
 			Program program = new Program (item);
 			program.UpdateQuality ();
 
-			Assert.AreEqual (6, item.Quality);
-			Assert.AreEqual (4, item.SellIn);
+			Assert.AreEqual (6, item.Quality, "First day");
+			Assert.AreEqual (4, item.SellIn, "First day");
+
+			program.UpdateQuality ();
+
+			Assert.AreEqual (5, item.Quality, "Second day");
+			Assert.AreEqual (3, item.SellIn, "Second day");
+		}
+
+		[Test]
+		public void RegularItemDecreasesTwoQualityPointperDayAfterExpires ()
+		{
+			Item item = CreateMockItem ("Elixir of the Mongoose", 0, 7);
+			
+			Program program = new Program (item);
+			program.UpdateQuality ();
+			
+			Assert.AreEqual (5, item.Quality, "last day");
+			Assert.AreEqual (-1, item.SellIn, "last day");
 		}
 
 		private Item CreateMockItem (string name, int sellIn, int quality)
