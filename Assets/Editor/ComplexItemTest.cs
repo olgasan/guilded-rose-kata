022913f56040b2960aeb32bb98fdaef5ba4e2c1b
Olga Cardenas <oh.cardenas@gmail.com>
2014-07-03 20:19:22 -0500

    SellInDecreasesByOneADayPasses

---
+++ Assets/Editor/ComplexItemTest.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+
+namespace UnityTest
+{
+	[TestFixture]
+	internal class ComplexItemTest
+	{
+		internal class ComplexItemMock : ComplexItem 
+		{
+			public ComplexItemMock (int si, int q) : base (si, q)
+			{
+
+			}
+
+			protected override bool CanModifySellIn 
+			{
+				get { return true; }
+			}
+		}
+
+		[Test]
+		public void SellInDecreasesByOneADayPasses ()
+		{
+			ComplexItem item = new ComplexItemMock (10, 0);
+			int prevSellIn = item.SellIn;
+
+			item.OnDayIncreased ();
+			Assert.That (item.SellIn, Is.LessThan (prevSellIn));
+		}
+	}
+}
